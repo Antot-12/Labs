0e152117aa2c735b992817071c1eca6a9f10c7ed
Antot_12 <34186135+Antot-12@users.noreply.github.com>
2019-10-23 23:16:54 +0300

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,6 +1,1 @@
 print ("Hello World") 
-slogan = str(input ("Write your sentence "))
-sentence = slogan [::-1]
-words = sentence.split()
-sentence_rev = " ".join(reversed(words))
-print (sentence_rev)
