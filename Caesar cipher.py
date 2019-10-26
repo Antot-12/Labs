f714a3033d596c206cc9b4930dfe0920d7f36288
Antot_12 <34186135+Antot-12@users.noreply.github.com>
2019-10-26 13:00:38 +0300

    Update Caesar cipher.py

--- Caesar cipher.py
+++ Caesar cipher.py
@@ -1,12 +1,21 @@
-i = 5
-while i < 15:
-    alpha = ' abcdefghijklmnopqrstuvwxyz0123456789QWERTYUIOPASDFGHJKLZXCVBNM'
-    step = 1
+i = True
+while i:
+    alpha = ' abcdefghijklmnopqrstuvwxyzQWERTYUIOPASDFGHJKLZXCVBNM0123456789'
+    step = 2
+    step1 = 1
     text = input("Please write your text ").strip()
     res = ''
+    digit=''
     for c in text:
         if c.isalpha():
-            res += alpha[(alpha.index(c) + step) % len(alpha)]
+          if digit:
+            res += str(int(digit)+int(step1))
+            digit = ''
+          res += alpha[(alpha.index(c) + step) % len(alpha)]
+        elif c.isnumeric():
+          digit += c
         else:
             res += c
+    if digit:
+      res += str(int(digit)+int(step1))
     print("Result: " + res + "")
