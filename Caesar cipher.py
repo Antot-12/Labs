6afa6654cce1746d9ac14198a003db5b963dd2ae
Antot_12 <anton.shyrko@gmail.com>
2019-11-10 23:34:56 +0200

    spaces fixed

--- Caesar cipher.py
+++ Caesar cipher.py
@@ -1,17 +1,22 @@
 i = True
 while i:
-    alpha = ' abcdefghijklmnopqrstuvwxyzQWERTYUIOPASDFGHJKLZXCVBNM0123456789'
+    alpha = ' abcdefghijklmnopqrstuvwxyzQWERTYUIOPASDFGHJKLZXCVBNMабвгдеєжзиіїйклмнопрстуфхцчшщьюяАБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЧШЩЬЮЯ0123456789'
     step = 2
     step1 = 1
     text = input("Please write your text: ").strip()
     res = ''
     digit = ''
     for c in text:
-        if c.isalpha() or c == ' ':
+        if c.isalpha():
             if digit:
                 res += str(int(digit) + int(step1))
                 digit = ''
             res += alpha[(alpha.index(c) + step) % len(alpha)]
+        elif c == ' ':
+            if digit:
+                res += str(int(digit) + int(step1))
+                digit = ''
+            res += c
         elif c.isnumeric():
             digit += c
         else:
